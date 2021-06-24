ee7137a1ba599d8519cef5a75452b105368a720b
foday <foday_s@yahoo.com>
2021-06-24 07:02:58 -0500

    Installed react via npx, set up firebase, create Navbar and Home.js files, and import both to the App.js file
    App.js is the root of the component and it is at the top while all the other components are imported to that root component. It is the first component that gets rendered to the DOM. The Navbar and Home components are exported to the root component. In the Navbar file a className named navbar and links are used for styling that block of the nav and the div. In the div block two links are created a Home link with just a / and a New Blog link with a /create link name

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+    <div className="navbar">
+        <h1>The Dojo Blog</h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <h1>App component</h1>
+        </div>
+
+    </div>  
+    );
+}
+ 
+export default Navbar;
