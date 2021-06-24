d13993e6ec8e49f53c9c600a0f69de590081605e
foday <foday_s@yahoo.com>
2021-06-24 13:23:15 -0500

    Outputting useState lists, using map
    map is used to cycle through all array using js in the template and output the template for  each one. The map method in js which cycles the array and something to each item in the array. return a bit of template for each item as we iterate through each one and then that template is going to the output to the browser. In the template a the blogs property with a map method is written in a template.

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,21 @@
 import { useState } from 'react';
 
 const Home = () => {
-    const [name, setName] = useState('mario');
+    const [blogs, setBlogs] = useState([
+        {title: 'My new React website', body: 'lorem ipsum....', author: 'mario', id: 1},
+        {title: 'Programming in c', body: 'c language is great for virus writing', author: 'sim', id: 2},
+        {title: 'Java', body: 'Java is a language for system building', author: 'Luigi', id: 3}
+    ]);
 
-
-    const handleClick = () => {
-        setName('luigi');
-    }
-
-    const handleClickAgain = (name) => {
-        console.log('hello '+ name);
-    }
     return (
-        <div>
-            <h1>Homepage</h1>
-            <p>{name}</p>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={() => {
-                handleClickAgain('Luigi')
-            }}>Click me again</button>
+        <div className="home">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p>
+                </div>
+    ))}
+           
         </div>
       );
 }
