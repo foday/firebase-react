ea8f1415dec1cf6deb9b321a28a9f24e8ece5a2d
foday <foday_s@yahoo.com>
2021-06-25 16:49:27 -0500

    useEffect function, add json data to db.json file and then db.json file to data folder, add fetch inside useEffect to fetch data
    useEffect runs a function every render of the component, the component renders initally when it first loads and it renders all of this to the dom but it also does it when there is a state change and when a value of a state is one of the useEffect dependency. In this code there is only an empty dependency cause there is an empty bracket and fetch is in the bracket of the useEffect, so it will run once or fetch the data then store it in the setBlogs const then in the template it will be expressed. The useEffect runs on every render.

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -8,15 +8,29 @@ const Home = () => {
         {title: 'Java', body: 'Java is a language for system building', author: 'Luigi', id: 3}
     ]);
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                setBlogs(data)
+            })
+    }, []);
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'sim')} title="Sim's list" handleDelete={handleDelete}/ >
+            {blogs && <BlogList blogs={blogs} title="All blogs listed!" handleDelete={handleDelete}/>}
+            {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'sim')} title="Sim's list" handleDelete={handleDelete}/ >}
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{name}</p>
         </div>
       );
 }
